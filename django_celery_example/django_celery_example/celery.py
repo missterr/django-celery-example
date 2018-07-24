efa2f11fe0f11259b7db8dc413b557c1882f9eb4
Копытов Максим <maxim.kopytov@3d4medical.com>
2018-07-24 17:06:49 +0300

    Init

---
+++ django_celery_example/django_celery_example/celery.py
@@ -0,0 +1,19 @@
+import os
+from celery import Celery
+from django.conf import settings
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_celery_example.settings')
+# os.environ.update(os.environ)
+
+app = Celery('django_celery_example')
+
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
